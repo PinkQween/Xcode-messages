c79d7937210079f97d6ac699d95fa3f57dfdbfe3
PinkQween <hanna_boone@icloud.com>
2023-01-13 16:24:20 -0700

    Blank Xcode project

---
+++ messages2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  messages2
+//
+//  Created by Hanna Skairipa on 1/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ messages2UITests/messages2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  messages2UITestsLaunchTests.swift
+//  messages2UITests
+//
+//  Created by Hanna Skairipa on 1/13/23.
+//
+
+import XCTest
+
+final class messages2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
