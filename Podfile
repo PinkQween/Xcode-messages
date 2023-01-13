d11a57201c62c8c75307433fc57ce5ef13df7db6
PinkQween <skairipa@pitchblack.tk>
2023-01-13 16:53:00 -0700

    Set up files and folders

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'messages2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for messages2
+
+  target 'messages2Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'messages2UITests' do
+    # Pods for testing
+  end
+
+end
--- messages2/Controllers/ConversationsViewController.swift
+++ messages2/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  messages2
 //
 //  Created by Hanna Skairipa on 1/13/23.
@@ -7,13 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ConversationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
